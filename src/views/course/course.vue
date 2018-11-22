e2730f40b97ad4d3371f9f2dd8b0f015e1e4e439
qw572386 <735520121@qq.com>
2018-11-22 08:59:44 +0800

    详情页设计

--- src/views/course/course.vue
+++ src/views/course/course.vue
@@ -1,12 +1,37 @@
 <template>
-    <div class="vit-course">
-        <div class=""></div>
-    </div>
+    <Row type="flex" justify="center" class="vit-course">
+        <Col span="20">
+            <Row class="vit-course-top">
+                <Col span="12" :style="{backgroundImage: 'url('+ courseImg +')'}" class="vit-course-top-img"></Col>
+                <Col span="11" offset="1"></Col>
+            </Row> 
+            <Tabs style="height: 500px">
+                <TabPane label="课程介绍" icon="ios-bookmarks">标签一的内容</TabPane>
+                <TabPane label="课程评价" icon="ios-bookmarks">暂无评价</TabPane>
+            </Tabs>
+            <Tabs>
+                <TabPane label="相关课程推荐" icon="ios-bookmarks">
+                    <vit-cate :cardList="cardList"  @on-select="view"></vit-cate>
+                </TabPane>
+            </Tabs>
+
+        </Col>
+    </Row>
 </template>
 
 <script>
 import { viewCourseDetail } from '@/api/course'
+import vitCate from '_c/vitCate'
 export default {
+    components: {
+        vitCate
+    },
+    data () {
+        return {
+            courseImg: 'https://img1.ph.126.net/PB4vJMMi3OdPd3U8YmrE1A==/6608179232143845685.jpg',
+            cardList: []
+        }
+    },
     created () {
         this.getCourseDetail()
     },
@@ -16,6 +41,9 @@ export default {
             viewCourseDetail(that.$route.params.id).then(res => {
                 console.log(res)
             })
+        },
+        view (id) {
+            console.log(id)
         }
     }
 }
